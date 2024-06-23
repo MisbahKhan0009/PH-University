0c532eb8dbfcd1c3f20f849498d631b0b38bcccf
MisbahKhan0009 <misbah.khan@northsouth.edu>
2024-06-23 13:51:50 +0600

    Joi Validation implemented

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,8 +1,115 @@
 import { Request, Response } from 'express';
 import { StudentServices } from './student.service';
+import Joi from 'Joi';
 
 const createStudent = async (req: Request, res: Response) => {
   try {
+    //creating a schema validation using joi
+
+    const userNameSchema = Joi.object({
+      firstName: Joi.string()
+        .trim()
+        .required()
+        .pattern(/^[A-Z][a-z]*$/)
+        .messages({
+          'string.pattern.base':
+            '{#label} must start with an uppercase letter.',
+          'any.required': 'First name is required.',
+        }),
+      middleName: Joi.string().trim().optional(),
+      lastName: Joi.string()
+        .trim()
+        .required()
+        .pattern(/^[a-zA-Z]+$/)
+        .messages({
+          'string.pattern.base':
+            '{#label} must contain only alphabetic characters.',
+          'any.required': 'Last name is required.',
+        }),
+    });
+
+    const guardianSchema = Joi.object({
+      fatherName: Joi.string().required().messages({
+        'any.required': "Father's name is required.",
+      }),
+      fatherContactNo: Joi.string().required().messages({
+        'any.required': "Father's contact number is required.",
+      }),
+      fatherOccupation: Joi.string().required().messages({
+        'any.required': "Father's occupation is required.",
+      }),
+      motherName: Joi.string().required().messages({
+        'any.required': "Mother's name is required.",
+      }),
+      motherContactNo: Joi.string().required().messages({
+        'any.required': "Mother's contact number is required.",
+      }),
+      motherOccupation: Joi.string().required().messages({
+        'any.required': "Mother's occupation is required.",
+      }),
+    });
+
+    const localGaurdianSchema = Joi.object({
+      name: Joi.string().required().messages({
+        'any.required': "Local guardian's name is required.",
+      }),
+      contactNo: Joi.string().required().messages({
+        'any.required': "Local guardian's contact number is required.",
+      }),
+      occupation: Joi.string().required().messages({
+        'any.required': "Local guardian's occupation is required.",
+      }),
+      address: Joi.string().required().messages({
+        'any.required': "Local guardian's address is required.",
+      }),
+    });
+
+    const studentSchema = Joi.object({
+      id: Joi.string().required().messages({
+        'any.required': 'Student id is required.',
+      }),
+      name: userNameSchema.required().messages({
+        'any.required': 'Student name is required.',
+      }),
+      gender: Joi.string().valid('male', 'female').required().messages({
+        'any.only': "{#label} is not supported. Use 'male' or 'female'.",
+        'any.required': 'Gender is required.',
+      }),
+      dateOfBirth: Joi.string(),
+      email: Joi.string().email().required().messages({
+        'string.email': '{#label} must be a valid email address.',
+        'any.required': 'Email address is required.',
+      }),
+      contactNo: Joi.string().required().messages({
+        'any.required': 'Contact number is required.',
+      }),
+      emergencyContactNo: Joi.string().required().messages({
+        'any.required': 'Emergency contact number is required.',
+      }),
+      bloodGroup: Joi.string()
+        .valid('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-')
+        .messages({
+          'any.only':
+            "{#label} is not supported. Use 'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'.",
+        }),
+      presentAddress: Joi.string().required().messages({
+        'any.required': 'Present address is required.',
+      }),
+      permanentAddress: Joi.string().required().messages({
+        'any.required': 'Permanent address is required.',
+      }),
+      guardian: guardianSchema.required().messages({
+        'any.required': 'Guardian information is required.',
+      }),
+      localGaurdian: localGaurdianSchema.required().messages({
+        'any.required': 'Local guardian information is required.',
+      }),
+      profileImg: Joi.string(),
+      isActive: Joi.string().valid('active', 'blocked').default('active'),
+    });
+
+    // end of creating a schema validation using joi
+
     const { student: studentData } = req.body;
     const result = await StudentServices.createStudentIntoDB(studentData);
 
@@ -12,7 +119,11 @@ const createStudent = async (req: Request, res: Response) => {
       data: result,
     });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+      error: error,
+    });
   }
 };
 
@@ -26,7 +137,11 @@ const getAllStudents = async (req: Request, res: Response) => {
       data: result,
     });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+      error: error,
+    });
   }
 };
 
@@ -41,7 +156,11 @@ const getSingleStudent = async (req: Request, res: Response) => {
       data: result,
     });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+      error: error,
+    });
   }
 };
 
--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -1,3 +1,4 @@
+import validator from 'validator';
 import { Schema, model, connect } from 'mongoose';
 import {
   Guardian,
@@ -9,96 +10,145 @@ import {
 const userNameSchema = new Schema<UserName>({
   firstName: {
     type: String,
-    required: true,
+    trim: true,
+    required: [true, 'First name is required.'],
+    validate: {
+      validator: function (value: string) {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return firstNameStr === value;
+      },
+    },
+    message: '{VALUE} is not a valid first name.',
   },
   middleName: {
     type: String,
+    trim: true,
     required: false,
   },
   lastName: {
     type: String,
-    required: true,
+    trim: true,
+    required: [true, 'Last name is required.'],
+    validate: {
+      validator: (value: string) => validator.isAlpha(value),
+      message: '{VALUE} is not a valid last name.',
+    },
   },
 });
+
 const guardianSchema = new Schema<Guardian>({
   fatherName: {
     type: String,
-    required: true,
+    required: [true, "Father's name is required."],
   },
   fatherContactNo: {
     type: String,
-    required: true,
+    required: [true, "Father's contact number is required."],
   },
   fatherOccupation: {
     type: String,
-    required: true,
+    required: [true, "Father's occupation is required."],
   },
   motherName: {
     type: String,
-    required: true,
+    required: [true, "Mother's name is required."],
   },
   motherContactNo: {
     type: String,
-    required: true,
+    required: [true, "Mother's contact number is required."],
   },
   motherOccupation: {
     type: String,
-    required: true,
+    required: [true, "Mother's occupation is required."],
   },
 });
 
 const LocalGaurdianSchema = new Schema<LocalGaurdian>({
   name: {
     type: String,
-    required: true,
+    required: [true, "Local guardian's name is required."],
   },
   contactNo: {
     type: String,
-    required: true,
+    required: [true, "Local guardian's contact number is required."],
   },
   occupation: {
     type: String,
-    required: true,
+    required: [true, "Local guardian's occupation is required."],
   },
   address: {
     type: String,
-    required: true,
+    required: [true, "Local guardian's address is required."],
   },
 });
 
 const studentSchema = new Schema<Student>({
-  id: { type: String },
-  name: userNameSchema,
-  gender: ['male', 'female'],
+  id: {
+    type: String,
+    required: [true, 'Student id is required.'],
+    unique: true,
+  },
+  name: {
+    type: userNameSchema,
+    required: [true, 'Student name is required.'],
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ['male', 'female'],
+      message: "{VALUE} is not supported. Use 'male' or 'female'.",
+    },
+    required: [true, 'Gender is required.'],
+  },
   dateOfBirth: {
     type: String,
   },
   email: {
     type: String,
-    required: true,
+    required: [true, 'Email address is required.'],
+    unique: true,
+    validate: {
+      validator: (value: string) => validator.isEmail(value),
+    },
   },
   contactNo: {
     type: String,
-    required: true,
+    required: [true, 'Contact number is required.'],
   },
   emergencyContactNo: {
     type: String,
-    required: true,
+    required: [true, 'Emergency contact number is required.'],
   },
-  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
-
+  bloodGroup: {
+    type: String,
+    enum: {
+      values: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+      message:
+        "{VALUE} is not supported. Use 'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'.",
+    },
+  },
   presentAddress: {
     type: String,
-    required: true,
+    required: [true, 'Present address is required.'],
   },
   permanentAddress: {
     type: String,
-    required: true,
+    required: [true, 'Permanent address is required.'],
   },
-  guardian: guardianSchema,
-  localGaurdian: LocalGaurdianSchema,
+  guardian: {
+    type: guardianSchema,
+    required: [true, 'Guardian information is required.'],
+  },
+  localGaurdian: {
+    type: LocalGaurdianSchema,
+    required: [true, 'Local guardian information is required.'],
+  },
   profileImg: { type: String },
-  isActive: ['active', 'blocked'],
+  isActive: {
+    type: String,
+    enum: ['active', 'blocked'],
+    default: 'active',
+  },
 });
 
 export const StudentModel = model<Student>('Student', studentSchema);
